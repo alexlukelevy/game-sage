07ef84fc90dea4ea6384b4ca96892d5f495d0374
Alex Levy <alex.levy@hotmail.co.uk>
2017-10-15 13:57:26 +0100

    Switching to multi-feature analysis - using themes and genres

--- recommender.py
+++ recommender.py
@@ -1,12 +1,40 @@
 import psycopg2
 import sys
+
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import FeatureUnion, Pipeline
+
+
+class Game:
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.properties = kwargs
+
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, games):
+        return map(lambda game: game.properties[self.key], games)
 
 
 def get_genres(data):
     try:
         genres = map(lambda g: g['name'], data['genres'])
+        return ','.join(genres)
+    except KeyError:
+        return ''
+
+
+def get_themes(data):
+    try:
+        genres = map(lambda g: g['name'], data['themes'])
         return ','.join(genres)
     except KeyError:
         return ''
@@ -21,32 +49,44 @@ def get_games(curr):
     curr.execute('SELECT data FROM games WHERE data->>\'reviews\' IS NOT NULL ORDER BY id DESC LIMIT 200')
     games = map(lambda row: row[0], curr.fetchall())
 
-    return list(map(lambda game: (get_name(game), get_genres(game)), games))
+    return list(map(lambda game: Game(get_name(game), genres=get_genres(game), themes=get_themes(game)), games))
 
 
 def cluster(games):
     def tokenizer(words):
         return words.split(',')
 
-    vectorizer = TfidfVectorizer(input='content', analyzer='word', tokenizer=tokenizer, lowercase=True)
-    # vectorize the genres
-    v = vectorizer.fit_transform(map(lambda game: game[1], games))
+    genre_pipeline = Pipeline([
+        ('selector', ItemSelector('genres')),
+        ('tfidf', TfidfVectorizer(input='content', analyzer='word', tokenizer=tokenizer, lowercase=True))
+    ])
 
-    km = KMeans(n_clusters=40)
-    km.fit(v)
+    theme_pipeline = Pipeline([
+        ('selector', ItemSelector('themes')),
+        ('tfidf', TfidfVectorizer(input='content', analyzer='word', tokenizer=tokenizer, lowercase=True))
+    ])
+
+    features = FeatureUnion(transformer_list=[('genre', genre_pipeline), ('theme', theme_pipeline)])
+
+    x = features.fit_transform(games)
+
+    n_clusters = 40
+
+    km = KMeans(n_clusters=n_clusters)
+    km.fit(x)
 
     print("Top terms per cluster:")
 
-    order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+    # order_centroids = km.cluster_centers_.argsort()[:, ::-1]
 
-    terms = vectorizer.get_feature_names()
+    # terms = features.get_feature_names()
     # print(terms)
 
-    for i in range(40):
-        print("Cluster %d:" % i, end='')
-        for ind in order_centroids[i, :10]:
-            print(' %s' % terms[ind], end='')
-        print()
+    # for i in range(n_clusters):
+    #     print("Cluster %d:" % i, end='')
+    #     for ind in order_centroids[i, :10]:
+    #         print(' %s' % terms[ind], end='')
+    #     print()
 
     # print(km.labels_)
 
@@ -58,7 +98,7 @@ def cluster(games):
         if cluster_num not in results:
             results[cluster_num] = []
 
-        results[cluster_num].append(game[0])
+        results[cluster_num].append(game.name)
 
     for key, value in results.items():
         print('Cluster {}'.format(key))
@@ -77,5 +117,5 @@ def run():
     curr.close()
     conn.close()
 
-run()
 
+run()
