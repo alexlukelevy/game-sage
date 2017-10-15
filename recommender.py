dd9ab00df77874d85ecb0bde2d5bba4718da2bdd
Alex Levy <alex.levy@hotmail.co.uk>
2017-10-15 15:01:27 +0100

    Tweaking k-means to include game concepts and adjusting weights

--- recommender.py
+++ recommender.py
@@ -24,6 +24,10 @@ class ItemSelector(BaseEstimator, TransformerMixin):
         return map(lambda game: game.properties[self.key], games)
 
 
+def get_name(data):
+    return data['name']
+
+
 def get_genres(data):
     try:
         genres = map(lambda g: g['name'], data['genres'])
@@ -34,25 +38,49 @@ def get_genres(data):
 
 def get_themes(data):
     try:
-        genres = map(lambda g: g['name'], data['themes'])
-        return ','.join(genres)
+        themes = map(lambda g: g['name'], data['themes'])
+        return ','.join(themes)
     except KeyError:
         return ''
 
 
-def get_name(data):
-    return data['name']
+def get_concepts(data):
+    try:
+        concepts = map(lambda g: g['name'], data['concepts'])
+        return ','.join(concepts)
+    except (KeyError, TypeError):
+        return ''
+
+
+def get_game(data):
+    return Game(
+        get_name(data),
+        genres=get_genres(data),
+        themes=get_themes(data),
+        concepts=get_concepts(data)
+    )
 
 
 def get_games(curr):
     # selecting more popular games
-    curr.execute('SELECT data FROM games WHERE data->>\'reviews\' IS NOT NULL ORDER BY id DESC LIMIT 200')
-    games = map(lambda row: row[0], curr.fetchall())
+    sql = '''
+SELECT data
+FROM games
+WHERE
+  data->>'reviews' IS NOT NULL
+  AND data->>'themes' IS NOT NULL
+  AND data->>'concepts' IS NOT NULL
+  AND data->>'genres' IS NOT NULL
+ORDER BY id DESC
+LIMIT 200
+'''
+    curr.execute(sql)
+    data = map(lambda row: row[0], curr.fetchall())
 
-    return list(map(lambda game: Game(get_name(game), genres=get_genres(game), themes=get_themes(game)), games))
+    return list(map(get_game, data))
 
 
-def cluster(games):
+def cluster(games, n_clusters):
     def tokenizer(words):
         return words.split(',')
 
@@ -66,29 +94,28 @@ def cluster(games):
         ('tfidf', TfidfVectorizer(input='content', analyzer='word', tokenizer=tokenizer, lowercase=True))
     ])
 
-    features = FeatureUnion(transformer_list=[('genre', genre_pipeline), ('theme', theme_pipeline)])
+    concept_pipeline = Pipeline([
+        ('selector', ItemSelector('concepts')),
+        ('tfidf', TfidfVectorizer(input='content', analyzer='word', tokenizer=tokenizer, lowercase=True))
+    ])
+
+    features = FeatureUnion(
+        transformer_list=[
+            ('genre', genre_pipeline),
+            ('theme', theme_pipeline),
+            ('concept', concept_pipeline),
+        ],
+        transformer_weights={
+            'genre': 1,
+            'theme': 0.2,
+            'concept': 0.3,
+        }
+    )
 
     x = features.fit_transform(games)
 
-    n_clusters = 40
-
-    km = KMeans(n_clusters=n_clusters)
+    km = KMeans(n_clusters=n_clusters, max_iter=10000)
     km.fit(x)
-
-    print("Top terms per cluster:")
-
-    # order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-
-    # terms = features.get_feature_names()
-    # print(terms)
-
-    # for i in range(n_clusters):
-    #     print("Cluster %d:" % i, end='')
-    #     for ind in order_centroids[i, :10]:
-    #         print(' %s' % terms[ind], end='')
-    #     print()
-
-    # print(km.labels_)
 
     results = dict()
 
@@ -112,7 +139,7 @@ def run():
     curr = conn.cursor()
 
     games = get_games(curr)
-    cluster(games)
+    cluster(games, 50)
 
     curr.close()
     conn.close()
